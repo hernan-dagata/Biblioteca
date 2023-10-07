a1db79bfaccaaa0ac5b286f5c207ac6730eb0e39
Hernan Garcia <hernandagata@gmail.com>
2023-10-07 12:48:44 -0500

    Se actualizan href en menu

--- views/template/header.php
+++ views/template/header.php
@@ -18,19 +18,19 @@
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav">
                 <li class="nav-item active">
-                    <a class="nav-link btn btn-secondary mr-2" href="/Biblioteca/index.php">Inicio</a>
+                    <a class="nav-link btn btn-secondary mr-2" href="/index.php">Inicio</a>
                 </li>
                 <li class="nav-item active">
-                    <a class="nav-link btn btn-secondary mr-2" href="/Biblioteca/views/gestionar_usuario.php">Gestionar usuarios</a>
+                    <a class="nav-link btn btn-secondary mr-2" href="/views/gestionar_usuario.php">Gestionar usuarios</a>
                 </li>
                 <li class="nav-item active">
-                    <a class="nav-link btn btn-secondary mr-2" href="/Biblioteca/views/gestionar_autor.php">Gestionar autores</a>
+                    <a class="nav-link btn btn-secondary mr-2" href="/views/gestionar_autor.php">Gestionar autores</a>
                 </li>
                 <li class="nav-item active">
-                    <a class="nav-link btn btn-secondary mr-2" href="/Biblioteca/views/gestionar_libro.php">Gestionar libros</a>
+                    <a class="nav-link btn btn-secondary mr-2" href="/views/gestionar_libro.php">Gestionar libros</a>
                 </li>
                 <li class="nav-item active">
-                    <a class="nav-link btn btn-secondary" href="/Biblioteca/views/gestionar_prestamo.php">Gestionar préstamos</a>
+                    <a class="nav-link btn btn-secondary" href="/views/gestionar_prestamo.php">Gestionar préstamos</a>
                 </li>
             </ul>
         </div>
