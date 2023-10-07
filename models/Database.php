d5f5c0edb307dfbc466fde68b44ffa8f0539daf0
Hernan Garcia <hernandagata@gmail.com>
2023-10-07 12:54:46 -0500

    Se agrega puerto para conexión a bd

--- models/Database.php
+++ models/Database.php
@@ -2,6 +2,7 @@
 
 class Database {
     private $host = "containers-us-west-89.railway.app";
+    private $port = 7037; 
     private $dbname = "railway";
     private $username = "root";
     private $password = "gS4gavPEqNXjETjUG326";
@@ -11,7 +12,7 @@ class Database {
         $this->conn = null;
 
         try {
-            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->dbname, $this->username, $this->password);
+            $this->conn = new PDO("mysql:host=" . $this->host . ";port=" . $this->port . ";dbname=" . $this->dbname, $this->username, $this->password);
             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         } catch(PDOException $exception) {
             echo "Connection error: " . $exception->getMessage();
